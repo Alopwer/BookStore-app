e531a0eb88d6ad9581731442606057e2261ae3b8
Nikita <collidem31@gmail.com>
2019-10-29 11:38:45 +0200

    added redux middleware, total functionality completed

--- src/reducers/shopping-cart.js
+++ src/reducers/shopping-cart.js
@@ -1,15 +1,19 @@
 const updateOrder = (state, bookId, quantity) => {
-    const { bookList: { books }, shoppingCart: { cartItems } } = state
+    const { bookList: { books }, shoppingCart: { cartItems, orderTotal } } = state
     const book = books.find(({ id }) => id === bookId)
     const itemIndex = cartItems.findIndex(({id}) => id === bookId)
     const item = cartItems[itemIndex]
 
     const newItem = updateCartItem(book, item, quantity)
-
+    const newCartItems = updateCartItems(cartItems, newItem, itemIndex)
     return {
-        orderTotal: 0,
-        cartItems: updateCartItems(cartItems, newItem, itemIndex)
+        cartItems: newCartItems,
+        orderTotal: updateTotal(newCartItems)
     }
+}
+
+const updateTotal = (cartItems) => {
+    return cartItems.reduce((prev, item) => prev + item.total, 0)
 }
 
 const updateCartItems = (cartItems, item, index) => {
